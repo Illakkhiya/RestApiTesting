5f8ec3606aa47beda5f5da37e1de1e3383109b58
Illakkhiya <illakkhiya@gmail.com>
2018-03-03 14:51:57 -0800

    Added TicketTest class in src Added dependencies for TestNg and RestAssured in pom.xml Added maven compiler plugin and surefire plugin in pom.xml created tesng.xml file

---
+++ src/test/java/zendesk/TicketTest.java
@@ -0,0 +1,15 @@
+package zendesk;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class TicketTest {
+	
+	@Test
+	public void setUp()
+	{
+		System.out.println("Hello World");
+	}
+
+}
